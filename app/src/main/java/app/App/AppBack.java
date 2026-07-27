ea38e4aee870beb31d19fd9f2fd6bd40c2f297ee
plaxir <emre79070@gmail.com>
2026-07-27 19:21:38 +0300

    Replaced with global empty Runnable

--- app/src/main/java/app/App/AppBack.java
+++ app/src/main/java/app/App/AppBack.java
@@ -1813,7 +1813,7 @@ public class AppBack extends AppWeb {
 
                     //if(timer.Get())
 
-                    loadOrLoadAndStart(mediaPlayer.isPlaying(),()->mediaPlayer.getSeekAfterIsPlayingDynamic(),()->{});
+                    loadOrLoadAndStart(mediaPlayer.isPlaying(),()->mediaPlayer.getSeekAfterIsPlayingDynamic(),StaticFunctions.Empty.r);
 
                     errorHandel.posSaved = false;
                     localGenerator.mediaError.started = false;
