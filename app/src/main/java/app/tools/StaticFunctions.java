f1e532e7593b44c24682d0be8967cab61f016f81
plaxir <emre79070@gmail.com>
2026-07-27 19:29:44 +0300

    Not using

--- app/src/main/java/app/tools/StaticFunctions.java
+++ app/src/main/java/app/tools/StaticFunctions.java
@@ -22,9 +22,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.os.Build;
-import android.view.View;
-import android.view.WindowInsets;
-import android.view.WindowInsetsController;
 
 import java.util.Date;
 import java.util.List;
